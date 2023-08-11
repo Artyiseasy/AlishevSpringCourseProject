0f6ca0e15203b7bdca855da2c6e925d99b74deeb
Artyom Tarasov <artem.tarasov15@gmail.com>
2023-08-11 16:24:58 +0300

    Подключена бд, изменены методы save и index в DAO

--- src/main/java/ru/tarasov/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/tarasov/springcourse/dao/PersonDAO.java
@@ -3,46 +3,86 @@ package ru.tarasov.springcourse.dao;
 import org.springframework.stereotype.Component;
 import ru.tarasov.springcourse.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    //этот класс будет общаться со списком(БД), извлекать людей из списка, находить по ID, обновлять, добавлять6 удалять
 
-        private List<Person> people;
         private static int PEOPLE_COUNT;
+        private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+        private static final String USERNAME = "postgres";
+        private static final String PASSWORD = "123";
 
+        private static Connection connection; // создаем для соеденения с бд
 
-    {
-        //блок инициализации
-        people = new ArrayList<>();
+        static { // убеждаемся, что у нас подключен драйвер постгреса
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e){
+                e.printStackTrace();
+            }
 
-        people.add(new Person(++PEOPLE_COUNT,"Bob", 25, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Tom", 44, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Igor", 32, "igor@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike",62, "mike@mail.ru"));
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); // подключаемся к бд
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
-    }
     public List<Person> index(){
+        // создаем лист в который будем класть считаных и бд людей
+        List <Person> people = new ArrayList<>();
+        // это тот объект, который содержит в себе sql запрос
+        // на этом объекте connection(на нашем соеденение) создаем объект запрос к бд
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            // resultSet - объект который инкапсулирует результат sql запроса
+            ResultSet resultSet =  statement.executeQuery(SQL); //statement выполняет SQL запрос
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
+
+
     public Person show (int id){
-       return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+   //    return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
     public void save(Person person){
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
-    }
-    public void update(int id, Person updatePerson){
-        Person personToBeUpdated = show(id);
+        //person.setId(++PEOPLE_COUNT);
+      //  people.add(person);
+        try {
+            Statement statement = connection.createStatement();
 
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+            //так делать нельзя, это временно
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName()+
+                    "', " + person. getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(SQL); // executeUpdate просто обновляет данные в базе, ничего не возвращает
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public void delete(int id){people.removeIf(p -> p.getId() == id);
+//  public void update(int id, Person updatePerson){
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setAge(updatePerson.getAge());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
+//    }
+   // public void delete(int id){people.removeIf(p -> p.getId() == id);
     }
-}
 
 
